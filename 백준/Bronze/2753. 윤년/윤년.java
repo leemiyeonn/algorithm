7510fb5e4049dd66c4f17469dca5c0485580466b
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-01-11 16:36:34 +0900

    [Bronze V] Title: 윤년, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -5,16 +5,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
         sc.close();
-
-        if (year % 4 == 0) {
-            if (year % 400 == 0) {
-                System.out.println(1);
-            }
-            else if (year % 100 == 0) {
-                System.out.println(0);
-            } else {
-                System.out.println(1);
-            }
+        
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println(1);
         } else {
             System.out.println(0);
         }
