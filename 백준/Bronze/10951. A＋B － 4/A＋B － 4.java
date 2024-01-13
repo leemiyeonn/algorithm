2020417379cf160098aa663b6ded5214a56f9d73
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-01-13 16:50:24 +0900

    [Bronze V] Title: A+B - 4, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,18 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-
-        while (sc.hasNextInt()) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int sum = a + b;
-
-            System.out.println(sum);
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        sc.close();
+        String line;
+        
+        while ((line = br.readLine()) != null) {
+            
+            String[] numbers = line.split(" ");
+            int n1 = Integer.parseInt(numbers[0]);
+            int n2 = Integer.parseInt(numbers[1]);
+            int sum = n1 + n2;
+            
+            bw.write(String.valueOf(sum));
+            bw.newLine();
+        }
+        bw.close();
     }
 }
