a117b0bdd5cfbd1cac8a245e8b2a315be52a6b6b
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-01-13 16:10:15 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            String line = bf.readLine();
+
+            if (line.isEmpty() || line.equals("0 0")) {
+                break;
+            }
+
+            int n1 = Integer.parseInt(line.split(" ")[0]);
+            int n2 = Integer.parseInt(line.split(" ")[1]);
+            int sum = n1 + n2;
+
+            bw.write(String.valueOf(sum));
+            bw.newLine();
+        }
+
+        bw.close();
+    }
+}
