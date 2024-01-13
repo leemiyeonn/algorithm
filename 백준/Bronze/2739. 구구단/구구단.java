eedff2cc05331b2506dd1356693dd124e0616ecf
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-01-13 14:01:39 +0900

    [Bronze V] Title: 구구단, Time: 244 ms, Memory: 18608 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(n1 + " * " + i + " = " + (n1 * i));
+        }
+    }
+}
