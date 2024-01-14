293d9fec456bce529a1033c5aa10b13756ed2d63
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-01-15 03:38:11 +0900

    [Bronze III] Title: 공 넣기, Time: 124 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input1 = br.readLine();
+        String[] arr1 = input1.split(" ");
+        int totalBasket = Integer.parseInt(arr1[0]);
+        int totalCount = Integer.parseInt(arr1[1]);
+
+        int[] basketArr = new int[totalBasket];
+
+        for (int i = 0; i < totalCount; i++) {
+            String input = br.readLine();
+            String[] arr = input.split(" ");
+
+            int startBasket = Integer.parseInt(arr[0]);
+            int endBasket = Integer.parseInt(arr[1]);
+            int ball = Integer.parseInt(arr[2]);
+
+            for (int j = startBasket; j <= endBasket; j++) {
+                basketArr[j - 1] = ball;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int ball : basketArr) {
+            result.append(ball);
+            result.append(" ");
+        }
+
+        bw.write(String.valueOf(result));
+        bw.close();
+    }
+}
