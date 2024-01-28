dc878fdc21609e92636ad2812d8a6e4e630143bc
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-01-29 02:16:00 +0900

    [Bronze I] Title: 소인수분해, Time: 168 ms, Memory: 15380 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                bw.write(String.valueOf(i));
+                bw.newLine();
+
+                n /= i;
+            }
+        }
+
+        br.close();
+        bw.close();
+
+    }
+}
