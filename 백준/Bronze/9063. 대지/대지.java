9d1e3ccce2d01c7ad108ff1327956aa7697cd4a6
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-02-01 00:39:53 +0900

    [Bronze III] Title: 대지, Time: 300 ms, Memory: 27188 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+
+        int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
+
+        for (int i = 0; i < count; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            minX = Math.min(x, minX);
+            maxX = Math.max(x, maxX);
+
+            minY = Math.min(y, minY);
+            maxY = Math.max(y, maxY);
+        }
+
+        int area = (maxX - minX) * (maxY - minY);
+
+        bw.write(String.valueOf(area));
+
+        br.close();
+        bw.close();
+    }
+}
