e0aae0c845cec2ef1708e602769771e99c95f0ec
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-02-02 18:59:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.46 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        
         HashMap<String, Integer> map = new HashMap<>();
         
         for (String p : participant) {
