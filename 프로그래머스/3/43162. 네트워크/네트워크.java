5c58f2bce7de5141486d8a34c7ee0226145c81c4
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-02-08 16:56:50 +0900

    [level 3] Title: 네트워크, Time: 0.10 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,30 @@
-import java.util.*;
-
 class Solution {
-    
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] visited = new boolean[computers.length];
+        int count = 0;
+        boolean[] visited = new boolean[n];
         
-        for (int i = 0; i < computers.length; i++) {
-            visited[i] = false;
-        }
-        
-        for (int i = 0; i < computers.length; i++) {
+        // 모든 노드 순회
+        for (int i = 0; i < n; i++) {
+            // 노드를 방문하지 않았으면
             if (!visited[i]) {
-                answer++;
+                // 새로운 네트워크 시작
+                count++;
+                // 다음 노드 탐색
                 dfs(i, computers, visited);
             }
         }
         
-        
-        return answer;
-}
-    public void dfs(int node, int[][] computers, boolean[] visited) {
+        return count;
+    }
+    
+    private void dfs(int node, int[][] computers, boolean[] visited) {
+        // 현재 노드 방문처리
         visited[node] = true;
-        
+
+        // 다음 노드 탐색
         for (int i = 0; i < computers.length; i++) {
-            if (!visited[i] && computers[node][i] == 1) {
+            if (!visited[i] && computers[node][i] != 0) {
+                // 방문하지 않았고, 다음노드와 연결되어 있는 노드 탐색
                 dfs(i, computers, visited);
             }
         }
