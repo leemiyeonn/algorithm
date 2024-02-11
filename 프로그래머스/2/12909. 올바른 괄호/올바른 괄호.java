5f1b5d38b1ae4d6492572af64547464df7889af9
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-02-11 23:10:33 +0900

    [level 2] Title: 올바른 괄호, Time: 19.34 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        char[] arr = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : arr) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
