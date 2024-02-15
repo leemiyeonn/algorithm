bd0a762806205516d2453b2140078293008ef121
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-02-15 19:15:53 +0900

    [level 1] Title: 모의고사, Time: 1.11 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int[] answers) {
+        
+        int[] score = new int[3];
+        int[] pattern1 = {1, 2, 3, 4, 5};
+        int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == pattern1[i % pattern1.length]) {
+                score[0]++;
+            }
+            if (answers[i] == pattern2[i % pattern2.length]) {
+                score[1]++;
+            }
+            if (answers[i] == pattern3[i % pattern3.length]) {
+                score[2]++;
+            }
+        }
+
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+
+        List<Integer> winnerList = new ArrayList<>();
+
+        for (int i = 0; i < score.length; i++) {
+            if (score[i] == maxScore) {
+                winnerList.add(i + 1);
+            }
+        }
+        
+        return winnerList;
+    }
+}
