09bfb3262e8dace7fd7e2b468fa1fcc0bfec95ff
leemiyeonn <120197266+leemiyeonn@users.noreply.github.com>
2024-07-31 10:33:35 +0900

    [level 1] Title: 평균 구하기, Time: 7.29 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,17 +1,8 @@
+import java.util.*;
+
 class Solution {
     public double solution(int[] arr) {
         
-        double answer = 0;
-        double sum = 0;
-        
-        for (int i = 0; i < arr.length; i++) {
-            
-          sum+=arr[i];   
-            
-        }
-        
-        answer = sum / arr.length;
-        
-        return answer;
+        return Arrays.stream(arr).average().getAsDouble();
     }
 }
